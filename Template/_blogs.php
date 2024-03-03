60125e66cc02ea37a25b8a2143a30f1b8af73ced
Anamika <anamika170304@gmail.com>
2024-03-03 18:20:43 +0530

    created blog-template

---
+++ Template/_blogs.php
@@ -0,0 +1,47 @@
+<!-- Blogs -->
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20">Latest Blogs</h4>
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">The Ultimate Guide to Mobile Shopping Bliss</h5>
+                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Welcome to the future of shopping—right at your
+                        fingertips! Mobile shopping has revolutionized the way we browse, choose, and buy our favorite
+                        products. If you haven't yet embraced the convenience of shopping on your mobile device, now is
+                        the perfect time to dive in.</p>
+                    <a href="https://www.omnisend.com/blog/mobile-commerce/" target="blank"
+                        class="color-second text-left">Go
+                        somewhere</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Top Trends in Mobile Fashion Shopping</h5>
+                    <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Fashion at your fingertips—literally! Mobile
+                        shopping has transformed the fashion landscape, bringing the latest trends to your mobile
+                        screen. Here's a peek into the top trends shaping the world of mobile fashion shopping.</p>
+                    <a href="https://techpacker.com/blog/design/top-10-fashion-technology-trends/" target="blank"
+                        class="color-second text-left">Go somewhere</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Unlocking the Potential: The Evolution of Mobile Phones</h5>
+                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Mobile phones have come a long way from being
+                        simple communication devices to becoming integral parts of our daily lives. Let's take a journey
+                        through the evolution of mobile phones and explore how they have transformed into powerful tools
+                        that connect, entertain, and empower us.</p>
+                    <a href="https://flauntdigital.com/blog/evolution-mobile-phones/" target="blank"
+                        class="color-second text-left">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- !Blogs -->
